d073f4ec4eaf060aca135c03cb2089c7c875c3f0
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2021-10-11 19:33:16 +0300

    Cleared clutter

--- airline/settings.py
+++ airline/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -124,5 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
 
